be7f943a1a8315932e8aa4690e667860b095924a
Giovanny Velez <giovannyvelez@hotmail.com>
2023-09-05 12:07:15 -0500

    Added chrome.events

---
+++ src/jsMain/kotlin/chrome/events/Event.kt
@@ -0,0 +1,14 @@
+package chrome.events
+
+import chrome.others.Callback
+
+external interface Event<T> {
+    fun <T> addListener(callback: Callback<T>)
+
+    fun addRules(rules: List<Rule>, callback: Callback<List<Rule>>?)
+    fun getRules(ruleIdentifiers: List<String>?, callback: Callback<List<Rule>>)
+    fun <T> hasListener(callback: Callback<T>): Boolean
+    fun hasListeners(): Boolean
+    fun <T> removeListener(callback: Callback<T>)
+    fun removeRules(ruleIdentifiers: List<String>?, callback: () -> Unit)
+}
---
+++ src/jsMain/kotlin/chrome/others/Typealiases.kt
@@ -0,0 +1,4 @@
+package chrome.others
+
+typealias Callback<T> = (input: T) -> Unit
+typealias JsNumber = Double
